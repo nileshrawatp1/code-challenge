9ffe5d61d1fd136ed409b407c1eb618a91b216e9
nileshrawatp1 <33788262+nileshrawatp1@users.noreply.github.com>
2024-11-19 12:02:36 +0530

    task-2

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,15 +1,38 @@
 import { List } from 'ui'
-
-const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
-fetch(api)
-    .then(response => response.json())
-    .then(data => console.log(data))
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+
+    const [list, setList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
+        fetch(api)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed To Fetch Pokemon");
+                }
+                return response.json()
+            })
+            .then((data) => {
+                setList(data.results);
+                setLoading(false)
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false)
+            })
+    }, [])
+
     return (
         <>
             <h1>Pokemon list:</h1>
-            <List />
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            {!loading && !error && <List data={list} />}
+
         </>
     )
 }
--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-export const List = () => {
-  return <p>List</p>
+export const List = ({ data }) => {
+    return (
+        <ul>
+            {data.map((pokemon, index) => (
+                <li key={index}>{pokemon.name}</li>
+            ))}
+        </ul>
+    )
 }
